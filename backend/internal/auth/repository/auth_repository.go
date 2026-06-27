0cd33a967ff97d8c96d56655d86484543d4d7624
Abhinav Kumar <abhinavkumar@Abhinavs-MacBook-Air.local>
2026-06-27 12:01:01 +0530

    feat(auth): add JWT service

--- backend/internal/auth/repository/auth_repository.go
+++ backend/internal/auth/repository/auth_repository.go
@@ -139,3 +139,31 @@ func (r *AuthRepository) FindByID(
 
 	return &user, nil
 }
+
+func (r *AuthRepository) ExistsByEmail(
+	ctx context.Context,
+	email string,
+) (bool, error) {
+
+	query := `
+	SELECT EXISTS(
+		SELECT 1
+		FROM users
+		WHERE email = $1
+	)
+	`
+
+	var exists bool
+
+	err := r.db.QueryRow(
+		ctx,
+		query,
+		email,
+	).Scan(&exists)
+
+	if err != nil {
+		return false, err
+	}
+
+	return exists, nil
+}
--- backend/internal/auth/service/auth_service.go
+++ backend/internal/auth/service/auth_service.go
@@ -1,6 +1,14 @@
 package service
 
 import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/abhinavkumar03/crm-lite/backend/internal/auth"
+	"github.com/abhinavkumar03/crm-lite/backend/internal/auth/dto"
 	"github.com/abhinavkumar03/crm-lite/backend/internal/auth/repository"
 )
 
@@ -15,4 +23,120 @@ func New(
 	return &AuthService{
 		repository: repository,
 	}
+}
+
+func (s *AuthService) hashPassword(
+	password string,
+) (string, error) {
+
+	bytes, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+func (s *AuthService) verifyPassword(
+	hash string,
+	password string,
+) error {
+
+	return bcrypt.CompareHashAndPassword(
+		[]byte(hash),
+		[]byte(password),
+	)
+}
+
+func (s *AuthService) Register(
+	ctx context.Context,
+	req dto.RegisterRequest,
+) (*auth.User, error) {
+
+	exists, err := s.repository.ExistsByEmail(
+		ctx,
+		req.Email,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if exists {
+		return nil, errors.New("email already exists")
+	}
+
+	hash, err := s.hashPassword(req.Password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user := &auth.User{
+		ID:           uuid.New(),
+		Name:         req.Name,
+		Email:        req.Email,
+		PasswordHash: hash,
+	}
+
+	err = s.repository.Create(
+		ctx,
+		user,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user.PasswordHash = ""
+
+	return user, nil
+}
+
+func (s *AuthService) Login(
+	ctx context.Context,
+	email string,
+	password string,
+) (*auth.User, error) {
+
+	user, err := s.repository.FindByEmail(
+		ctx,
+		email,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if user == nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	err = s.verifyPassword(
+		user.PasswordHash,
+		password,
+	)
+
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	user.PasswordHash = ""
+
+	return user, nil
+}
+
+func (s *AuthService) Profile(
+	ctx context.Context,
+	id string,
+) (*auth.User, error) {
+
+	return s.repository.FindByID(
+		ctx,
+		id,
+	)
 }
